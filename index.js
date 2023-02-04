ec97e2714abf81105acaf7ba4f4064bee546d26d
Henry <henrysungyun@gmail.com>
2023-02-04 13:28:29 -0800

    yayy! it get easier

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOne) {
+    return arrayOne.slice(0, 2);
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(arrayOne) {
+    return arrayOne.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier
+    };
+}
+
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(arrayOne, fn) {
+    if (fn === returnFirstTwoDrivers) {
+        return arrayOne.slice(0,2)
+    } else if (fn === returnLastTwoDrivers) {
+        return arrayOne.slice(2, 4)
+    };
+}
